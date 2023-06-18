1403e2b92313938161f8ff7cfd4ded0d14159715
panther711 <jameshan044@gmail.com>
2023-06-19 01:36:06 +0900

    fix:sidenav

--- src/components/Sidenav/index.jsx
+++ src/components/Sidenav/index.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import {
   CDBSidebar,
   CDBSidebarContent,
-  CDBSidebarHeader,
   CDBSidebarMenu,
-  CDBSidebarMenuItem,
 } from 'cdbreact';
 import { NavLink } from 'react-router-dom';
 import './index.css';
